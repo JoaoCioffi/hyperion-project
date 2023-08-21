efee1322a42831fff52f5bc6ac1a704e20c2070a
JoaoCioffi <joaorcioffi@gmail.com>
2023-08-21 17:34:23 -0300

    updated content for ETL Procedures

--- miscellaneous/ingestao-dados/main.py
+++ miscellaneous/ingestao-dados/main.py
@@ -1,9 +1,14 @@
+from serviceInfo import screenContent
 from invokeBuckets import Banks,\
                           Complaints,\
                           Employees
 
 # Calling methods
 def etlProcedures():
+
+    print('_'*35)
+    print(screenContent())
+    print('_'*35)
 
     # extract (loading raw dataframes)
     df_banks=Banks.loadBucketContent()
